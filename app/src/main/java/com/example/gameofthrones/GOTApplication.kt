190d6f23317df9a95d57151abc44666bec863e92
Sid <sidgowda.9@gmail.com>
2020-02-01 20:36:59 -0500

    added Application and Factory builder for Dagger

---
+++ app/src/main/java/com/example/gameofthrones/GOTApplication.kt
@@ -0,0 +1,11 @@
+package com.example.gameofthrones
+
+import android.app.Application
+import com.example.gameofthrones.di.GOTComponent
+
+class GOTApplication : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+    }
+}
--- app/src/main/java/com/example/gameofthrones/di/GOTComponent.kt
+++ app/src/main/java/com/example/gameofthrones/di/GOTComponent.kt
@@ -1,8 +1,14 @@
 package com.example.gameofthrones.di
 
+import android.app.Application
+import dagger.BindsInstance
 import dagger.Component
 
 @Component
 interface GOTComponent {
 
+    @Component.Factory
+    interface Factory {
+        fun create(@BindsInstance app: Application): GOTComponent
+    }
 }
