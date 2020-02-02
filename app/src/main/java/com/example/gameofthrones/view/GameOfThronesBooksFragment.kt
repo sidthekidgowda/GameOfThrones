685c87699425300f0c9acda99e43fc62bf4cceb3
Sid <sidgowda.9@gmail.com>
2020-02-02 16:52:56 -0500

    added fragment for books

---
+++ app/src/main/java/com/example/gameofthrones/view/GameOfThronesBooksFragment.kt
@@ -0,0 +1,30 @@
+package com.example.gameofthrones.view
+
+import android.content.Context
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+class GameOfThronesBooksFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        //@TODO set up view
+        return super.onCreateView(inflater, container, savedInstanceState)
+    }
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        //@TODO - set up dagger
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        //@TODO - set up view model
+    }
+}
