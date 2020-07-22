313c1c324af76014f35e3e57e093496e6f7293f0
Sid Gowda <sidgowda.9@gmail.com>
2020-07-22 15:36:18 -0400

    changed dependencies

--- app/src/main/java/com/example/gameofthrones/application/GameOfThronesApplication.kt
+++ app/src/main/java/com/example/gameofthrones/application/GameOfThronesApplication.kt
@@ -1,10 +1,11 @@
 package com.example.gameofthrones.application
 
+import android.app.Application
 import androidx.multidex.MultiDexApplication
 import com.example.gameofthrones.di.DaggerGameOfThronesComponent
 import com.example.gameofthrones.di.GameOfThronesComponent
 
-class GameOfThronesApplication : MultiDexApplication() {
+class GameOfThronesApplication : Application() {
 
     val component: GameOfThronesComponent by lazy {
         DaggerGameOfThronesComponent.factory().create(this)
