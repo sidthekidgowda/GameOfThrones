769e78f16f3f5ba66ccbc961ae0bd08968458f5d
Sid Gowda <sidgowda.9@gmail.com>
2020-07-22 18:16:08 -0400

    Using mockK to mock objects. finished testing datasource

--- app/src/test/java/com/example/gameofthrones/datasource/GameOfThronesDataSourceTest.kt
+++ app/src/test/java/com/example/gameofthrones/datasource/GameOfThronesDataSourceTest.kt
@@ -3,51 +3,66 @@ package com.example.gameofthrones.datasource
 import com.android.rxviewmodelutility.RxSchedulerRule
 import com.example.gameofthrones.model.Book
 import com.example.gameofthrones.room.BooksDao
-import com.example.gameofthrones.room.BooksDatabase
 import com.example.gameofthrones.service.GameOfThronesService
+import io.mockk.MockKAnnotations
+import io.mockk.every
+import io.mockk.impl.annotations.MockK
+import io.mockk.verify
 import io.reactivex.Flowable
 import io.reactivex.Single
-import org.intellij.lang.annotations.Flow
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
-import org.junit.runner.RunWith
-import org.mockito.ArgumentMatchers
-import org.mockito.Mock
-import org.mockito.Mockito.*
-import org.mockito.junit.MockitoJUnitRunner
 
-@RunWith(MockitoJUnitRunner::class)
 class GameOfThronesDataSourceTest {
 
     @get:Rule
     val rxRule = RxSchedulerRule()
 
-    @Mock
+    @MockK
     private lateinit var gameOfThronesService: GameOfThronesService
-    @Mock
+    @MockK
     private lateinit var booksDao: BooksDao
 
     private lateinit var dataSource: GameOfThronesDataSource
 
     @Before
     fun setup() {
+        MockKAnnotations.init(this, relaxUnitFun = true)
         dataSource = GameOfThronesDataSourceImpl(gameOfThronesService, booksDao)
-        `when`(gameOfThronesService.getListOfBooks()).thenReturn(Single.just(getListOfBooks()))
     }
 
     @Test
     fun `test no books in database make service calls and returns list of books`() {
-        `when`(booksDao.getBooks()).thenReturn(Flowable.empty())
-
+        every { booksDao.getBooks() } returns Flowable.empty()
+        every { gameOfThronesService.getListOfBooks() } returns Single.just(getListOfBooks())
         dataSource.getListOfBooks().test().assertValue(getListOfBooks())
     }
 
     @Test
-    fun `test no books in database make service calls and updates database`() {
-        `when`(booksDao.getBooks()).thenReturn(Flowable.empty())
-        dataSource.getListOfBooks().test().assertValue(getListOfBooks())
+    fun `test get books returns error on service call`() {
+        val throwable = Throwable()
 
+        every { booksDao.getBooks() } returns Flowable.empty()
+        every { gameOfThronesService.getListOfBooks() } returns Single.error(throwable)
+
+        dataSource.getListOfBooks().test().assertError(throwable)
+    }
+
+    @Test
+    fun `test get books makes service call and updates booksDao`() {
+        every { booksDao.getBooks() } returns Flowable.empty()
+        every { booksDao.insertAll(any()) } returns Unit
+        every { gameOfThronesService.getListOfBooks() } returns Single.just(getListOfBooks())
+        dataSource.getListOfBooks().test()
+        verify(exactly = 1) { booksDao.getBooks() }
+        verify(exactly = 1) { booksDao.insertAll(any())}
+    }
+
+    @Test
+    fun `test getBooks returns From database`() {
+        every { booksDao.getBooks() } returns Flowable.just(getListOfBooks())
+        dataSource.getListOfBooks().test().assertValue(getListOfBooks())
     }
 
     private fun getListOfBooks(): List<Book> {
